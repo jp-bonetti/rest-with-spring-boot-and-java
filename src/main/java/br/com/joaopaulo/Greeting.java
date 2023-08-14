e4b8416754bc5b9ef438dc3aa9a3fc0d9577b8fc
João Paulo Sigieski Bonetti <joaopaulobonetti2004@gmail.com>
2023-08-14 01:10:04 -0300

    Primeira classe criada

---
+++ src/main/java/br/com/joaopaulo/Greeting.java
@@ -0,0 +1,14 @@
+package br.com.joaopaulo;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
+
+
